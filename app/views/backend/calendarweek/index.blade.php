a186b54e8d65f685c6d6b0f01e6f3c153cbf07f9
Bernd Obendorfer <bernd.obendorfer@chello.at>
2014-08-06 15:09:13 +0200

    Calendar Week Part 1

--- app/views/backend/calendarweek/index.blade.php
+++ app/views/backend/calendarweek/index.blade.php
@@ -24,9 +24,69 @@
  </div>      
          <br>
          <br>
-         <br>
-         <div class="table-responsive">
+         <br>dd
+       
+
+
+<?php
+
+printf ('<h4>Diese Woche
+Mo, der %s bis Fr, der %s</h4>',  
+    date('d.m.Y', strtotime("last monday")), 
+    date('d.m.Y', strtotime("friday"))); 
+
+printf ('<h4>Nächste Woche
+Mo, der %s bis Ff, der %s</h4>',  
+    date('d.m.Y', strtotime("monday")), 
+    date('d.m.Y', strtotime("monday +5 days"))); 
+
+printf ('<h4>Übernächste Woche
+Mo, der %s bis Fr, der %s</h4>',  
+    date('d.m.Y', strtotime("next monday")), 
+    date('d.m.Y', strtotime("friday"))); 
+
+
+
+
+$start = new DateTime();
+$startendweek = new DateTime();
+$end = new DateTime("+3 weeks");
+$endendweek = new DateTime("+3 weeks");
+$interval = DateInterval::createFromDateString("7 day");
+
+$period = new DatePeriod($start,$interval,$end);
+$periodenweek = new DatePeriod($start,$interval,$end);
+foreach($period as $date){
+    echo $date->format('d.m.Y - W').'<br/>';
+}  
+
+
+
+
+
+
+
+
+$today = date("W"); // = z.B. 17 
+
+
+echo"$today";
+
+
+
+
+
+?><br>
+<?php
+// Wochennamen ausgeben
+
+setLocale(LC_ALL, "de_DE", "german");
+for ($zaehler = 5; $zaehler < 12; $zaehler++) {
+ echo strftime("%A, ", mktime(0, 0, 0, 1, $zaehler, 1970));
+}
+?>
+
          
-</div>
+
 </div>
 @stop
